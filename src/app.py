f0a26b8ce9ac0d6e5f2cca6e762024e5318376a0
amanbinepal <binepal2003@gmail.com>
2026-02-13 17:29:47 -0800

    modified shiny UI dashboard to align with sketch.png

--- src/app.py
+++ src/app.py
@@ -4,21 +4,33 @@ from shiny import App, ui
 
 # UI
 app_ui = ui.page_fillable(
-    ui.panel_title("Restaurant tipping"),
+    ui.panel_title("Supply Chain Dashboard"),
     ui.layout_sidebar(
         ui.sidebar("sidebar inputs", open="desktop"),
+        
         ui.layout_columns(
-            ui.value_box("Total tippers", "Value 1"),
-            ui.value_box("Average tip", "Value 2"),
-            ui.value_box("Average bill", "Value 3"),
-            fill=False,
-        ),
-        ui.layout_columns(
-            ui.card(ui.card_header("Tips data"), full_screen=True),
-            ui.card(ui.card_header("Total bill vs tip"), full_screen=True),
+            ui.card(ui.card_header("Map of route coverage across India"), full_screen=True),
+            ui.card(ui.card_header("Defect rate per sku"), full_screen=True),
             col_widths=[6, 6],
         ),
-        ui.layout_columns(ui.card(ui.card_header("Tip percentages"), full_screen=True)),
+
+        ui.layout_columns(
+            ui.card(ui.card_header("Customer demographic breakdown"), full_screen=True),
+            ui.card(ui.card_header("Product type availability breakdown"), full_screen=True),
+            ui.card(ui.card_header("Best sellers"), full_screen=True),
+            col_widths=[3, 3, 6],
+        ),
+
+        ui.layout_columns(
+            ui.card(ui.card_header("Cost per unit"), full_screen=True),
+            ui.card(ui.card_header("Overall inspection pass rate"), full_screen=True),
+            ui.card(
+                ui.card_header("Filters"),
+                ui.p("Filter for product category"),
+                ui.p("Filter for supplier"),
+            ),
+            col_widths=[3, 3, 6],
+        ),
     ),
 )
 
